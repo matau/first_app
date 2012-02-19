d14b1147e93e2dc4576877963bb309f3efc45b1f
Song Phommahaxay <song@Songs-MacBook-Air.local>
2012-02-19 15:35:30 +0100

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'a0ee4f4a52607a4f1347af66d53fece1f91deebf73b6408f1aeae87cc5caf2f4e8961e81851118123f036853b4ca9d3298c7ea4241f521c881b760bd5da203b7'
